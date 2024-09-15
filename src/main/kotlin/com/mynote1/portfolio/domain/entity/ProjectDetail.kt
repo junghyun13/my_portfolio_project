c90acfc908cfc1c606905c0d07e63cc688b9a67f
junghyun13 <ljh041314@gmail.com>
2024-09-15 20:31:53 +0900

    앤티티 개발 - 연관있음

--- src/main/kotlin/com/mynote1/portfolio/domain/entity/ProjectDetail.kt
+++ src/main/kotlin/com/mynote1/portfolio/domain/entity/ProjectDetail.kt
@@ -1,11 +1,25 @@
 package com.mynote1.portfolio.domain.entity
 
+
 import jakarta.persistence.*
 
 @Entity
-class ProjectDetail : BaseEntity() {
+class ProjectDetail(content: String, url: String?, isActive: Boolean) : BaseEntity() {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_detail_id")
     var id: Long? = null
+
+    var content: String = content
+
+    var url: String? = url
+
+    var isActive: Boolean = isActive
+
+    fun update(content: String, url: String?, isActive: Boolean) {
+        this.content = content
+        this.url = url
+        this.isActive = isActive
+    }
 }
--- src/main/kotlin/com/mynote1/portfolio/domain/entity/ProjectSkill.kt
+++ src/main/kotlin/com/mynote1/portfolio/domain/entity/ProjectSkill.kt
@@ -3,9 +3,19 @@ package com.mynote1.portfolio.domain.entity
 import jakarta.persistence.*
 
 @Entity
-class ProjectSkill : BaseEntity() {
+class ProjectSkill(project: Project, skill: Skill) : BaseEntity() {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_skill_id")
     var id: Long? = null
+
+    @ManyToOne(targetEntity = Project::class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false) //join의 기준이 프로젝트 아이디이다.
+    var project: Project = project
+
+    @ManyToOne(targetEntity = Skill::class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "skill_id", nullable = false)
+    var skill: Skill = skill
+
 }
